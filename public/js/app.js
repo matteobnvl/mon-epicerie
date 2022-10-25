44274ab86f21c39a903a8b7d01dbb0122019b8cd
matteobnvl <matteobonneval19@gmail.com>
2022-10-25 11:58:52 +0200

    barre search animation

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,30 @@
 var className = "active";
 var scrollTrigger = 20;
 
+var classNameSearch = "search-ville-active";
+var scrollSearch = 450;
+
 window.addEventListener('scroll',function(){
     if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
         document.getElementsByTagName("header")[0].classList.add(className);
     } else {
         document.getElementsByTagName("header")[0].classList.remove(className);
     }
+    if (window.scrollY >= scrollSearch || window.pageYOffset >= scrollSearch) {
+        document.getElementById("SearchVille").classList.add(classNameSearch);
+    } else {
+        document.getElementById("SearchVille").classList.remove(classNameSearch);
+    }
 })
 
+
+document.getElementById("SearchVille").addEventListener('input', function(){
+    if(this.value != ""){
+        document.getElementById("btn-search-ville").classList.add('btn-search-active');
+    }else{
+        document.getElementById("btn-search-ville").classList.remove('btn-search-active');
+    }
+});
 
 function BurgerActive(){
     document.getElementById('navburger').classList.toggle('active-burger');
